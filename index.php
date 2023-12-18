3804a9cb922d60b7e569bfd69c80c074802be618
C-Kenway <andresh35@outlook.com>
2023-12-17 20:15:07 -0600

    CorreccionParcial

--- index.php
+++ index.php
@@ -13,75 +13,71 @@ $boletaErr = $nameErr =  $firstlsErr = $secondlsErr = $emailErr = $passErr = $im
 $boleta = $name = $firstls = $secondls = $email = $pass = $img = $imgCadena = "";
 	
 //Auntenticarse en la BD
-$servername = "direccion.ip.del.servidor";
-$username = "usuario";
-$password = "password";
-$dbname = "school";
+$servername = "localhost/phpmyadmin";
+$username = "root";
+$password = "";
+$dbname = "sarpa";
  
 // Create connection
-$conn = new mysqli($servername,
-    $username, $password, $dbname);
+$conn = new mysqli($servername,$username, $password, $dbname);
  
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: "
-        . $conn->connect_error);
-}
+if ($conn->connect_error) {die("Connection failed: ". $conn->connect_error); }
  
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") { //Si esta madre no sirve ponle un GET <------- RECORDAR BORRARLO ANTES DE PRESENTAR AL PROFE
   
-  if (empty($_GET["boleta"])) {
+  if (empty($_POST["boleta"])) {
     $boletaErr = "El numero de boleta es requerido";
   } else {
-    $boleta = test_input($_GET["boleta"]);
+    $boleta = test_input($_POST["boleta"]);
   }
   
 
-  if (empty($_GET["name"])) {    
+  if (empty($_POST["nombre"])) {    
 	  $nameErr = "El nombre es requerido";
   } else {
-    $name = test_input($_GET["name"]);    
+    $name = test_input($_POST["nombre"]);    
   }
 
-  if (empty($_GET["firstls"])) {
+  if (empty($_POST["paterno"])) {
     $firstlsErr = "El primer apellido es requerido";
   }else {
-    $firstls = test_input($_GET["firstls"])
+    $firstls = test_input($_POST["paterno"]);
   }
   
-  if (empty($_GET["secondls"])) {
+  if (empty($_POST["materno"])) {
     $secondlsErr = "El segundo apellido es requerido";
   }else {
-    $firstls = test_input($_GET["secondls"])
+    $secondls = test_input($_POST["materno"]);
   }
 
-  if (empty($_GET["email"])) {
+  if (empty($_POST["email"])) {
     $emailErr = "El correo es requerido";
   } else {
-    $email = test_input($_GET["email"]);
+    $email = test_input($_POST["email"]);
     // Verificar si es un correo correcto
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Formato de correo no válido";
     }
   }
 
-  if (empty($_GET["pass"])) {
+  if (empty($_POST["pass"])) {
     $pass = "";
   } else {
-    $pass = test_input($_GET["pass"]);
+    $pass = test_input($_POST["pass"]);
   }
 
-  if(isset($_FILES["img"]) && $_FILES["img"]["imgErr"] == 0){
+  if(isset($_FILES["img"]) && $_FILES["img"]["error"] == 0){
     $img = $_FILES["img"]["tmp_name"];
     $imgCadena = base64_encode(file_get_contents($img));
-    echo "Imagen como cadena: " . $imgCadena
+    echo "Imagen como cadena: " . $imgCadena;
   }else {
     echo "Error al cargar la imagen";
   }
 
 #Ingresar los datos en la base de datos
-	$sql = "INSERT INTO alumnos(boleta, name, firstls, secondls, email, pass, imgCadena) VALUES
+	$sql = "INSERT INTO alumnos(boleta, nombre, firstls, secondls, email, pass, imgCadena) VALUES
     	( '".$boleta."', '".$name."', '".$firstls."', '".$secondls."', '".$email."','".$pass."', '".$imgCadena."')";
  
 	if ($conn->query($sql) === TRUE) {
@@ -93,9 +89,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { //Si esta madre no sirve ponle un GE
 // Cerrar la conexión.
 $conn->close();
 }
-
-
-	
 	
 function test_input($data) {
   $data = trim($data);
@@ -110,8 +103,6 @@ echo "";
 echo $name;
 echo "<br>";
 echo $email;
-echo "<br>";
-echo $age;
 echo "<br>";
 echo $pass;
 ?>
