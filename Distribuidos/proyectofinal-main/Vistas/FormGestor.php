805937017bffb99eb0b88deb43e41e000671147d
KuzeDavid <pain.swordartonline@gmail.com>
2023-12-20 15:41:34 -0600

    Muestra boleta y materia selectores y postea correctamente

--- Distribuidos/proyectofinal-main/Vistas/FormGestor.php
+++ Distribuidos/proyectofinal-main/Vistas/FormGestor.php
@@ -1,3 +1,21 @@
+<?php
+//Auntenticarse en la BD
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sarpa";
+ 
+// Create connection
+$conn = new mysqli($servername,
+    $username, $password, $dbname);
+ 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: "
+        . $conn->connect_error);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,11 +42,38 @@
 
     <!-- Formulario para actualizar calificaciones -->
     <form action="../php/actualizar_calificacion.php" method="POST" enctype="multipart/form-data">
-        <label for="boleta_calificar">Boleta del Alumno:</label>
+        <!--<label for="boleta_calificar">Boleta del Alumno:</label>
         <input type="text" name="boleta_calificar" id="boleta_calificar" placeholder="Ingrese la boleta del alumno">
 
         <label for="nombre_materia_calificar">Nombre de la Materia:</label>
-        <input type="text" name="id_materia_calificar" id="id_materia_calificar" placeholder="Ingrese el nombre de la materia">
+        <input type="text" name="id_materia_calificar" id="id_materia_calificar" placeholder="Ingrese el nombre de la materia"> -->
+
+
+        <label for="boleta_calificar">Boleta del Alumno:</label>
+        <select name="boleta_calificar" id="boleta_calificar">
+            <?php
+                // Consulta SQL para obtener la lista de boletas desde la tabla alumnos
+                $query_boletas = mysqli_query($conn, "SELECT boleta FROM alumnos");
+
+                // Iterar sobre los resultados y generar opciones del menú desplegable
+                while($row_boleta = mysqli_fetch_array($query_boletas)) {
+                    echo "<option value='".$row_boleta['boleta']."'>".$row_boleta['boleta']."</option>";
+                }
+            ?>
+        </select>
+
+        <label for="id_materia_calificar">Nombre de la Materia:</label>
+        <select name="id_materia_calificar" id="id_materia_calificar">
+            <?php
+                // Consulta SQL para obtener la lista de materias desde la base de datos
+                $query_materias = mysqli_query($conn, "SELECT id_materia, nombre FROM materias");
+
+                // Iterar sobre los resultados y generar opciones del menú desplegable
+                while($row_materia = mysqli_fetch_array($query_materias)) {
+                    echo "<option value='".$row_materia['id_materia']."'>".$row_materia['nombre']."</option>";
+                }
+            ?>
+        </select>
 
         <label for="calificacion_parcial1">Calificación Parcial 1:</label>
         <input type="text" name="calificacion_parcial1" id="calificacion_parcial1" placeholder="Ingrese la calificación del parcial 1">
