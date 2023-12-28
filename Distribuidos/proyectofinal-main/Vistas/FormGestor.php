233142b27cc117dc4502281b98b051c94c04c1db
Angel Vargas <angelvargasvelez21@gmail.com>
2023-12-27 19:17:46 -0600

    estilos 3: update formGest

--- Distribuidos/proyectofinal-main/Vistas/FormGestor.php
+++ Distribuidos/proyectofinal-main/Vistas/FormGestor.php
@@ -1,91 +1,155 @@
 <?php
-//Auntenticarse en la BD
+// Autenticarse en la BD
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "sarpa";
- 
-// Create connection
-$conn = new mysqli($servername,
-    $username, $password, $dbname);
- 
-// Check connection
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
 if ($conn->connect_error) {
-    die("Connection failed: "
-        . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionar Alumnos</title>
     <link rel="stylesheet" href="../css/formularios.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h2 {
+            text-align: center;
+            color: #333;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+
+        label {
+            margin-top: 10px;
+            margin-bottom: 5px;
+            color: #555;
+        }
+
+        input,
+        select {
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        button {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+        p.message,
+        p.error {
+            margin-top: 10px;
+            color: #333;
+            font-weight: bold;
+        }
+
+        .select-container {
+            display: flex;
+            justify-content: space-between;
+        }
+    </style>
 </head>
+
 <body>
 
-    <h2>Gestionar Calificaciones y Asignar Materias a Alumnos</h2>
+    <div class="container">
+        <h2>Gestionar Calificaciones y Asignar Materias a Alumnos</h2>
 
-    <!-- Formulario para asignar materias 
-    <form action="asignar_materia.php" method="POST">
-        <label for="boleta_asignar">Boleta del Alumno:</label>
-        <input type="text" name="boleta_asignar" id="boleta_asignar" placeholder="Ingrese la boleta del alumno">
+        <!-- Formulario para actualizar calificaciones -->
+        <form action="../php/actualizar_calificacion.php" method="POST" enctype="multipart/form-data" class="form">
+            <div class="select-container">
+                <div>
+                    <label for="boleta_calificar">Boleta del Alumno:</label>
+                    <select name="boleta_calificar" id="boleta_calificar">
+                        <?php
+                        // Consulta SQL para obtener la lista de boletas desde la tabla alumnos
+                        $query_boletas = mysqli_query($conn, "SELECT boleta FROM alumnos");
 
-        <label for="nombre_materia_asignar">Nombre de la Materia:</label>
-        <input type="text" name="nombre_materia_asignar" id="nombre_materia_asignar" placeholder="Ingrese el nombre de la materia">
+                        // Iterar sobre los resultados y generar opciones del menú desplegable
+                        while ($row_boleta = mysqli_fetch_array($query_boletas)) {
+                            echo "<option value='" . $row_boleta['boleta'] . "'>" . $row_boleta['boleta'] . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
 
-        <button type="submit" name="btn_asignar_materia">Asignar Materia</button>
-        <p><?php if(isset($_GET['asignacion_result'])){ echo $_GET['asignacion_result']; } ?></p>
-    </form>-->
+                <div>
+                    <label for="id_materia_calificar">Nombre de la Materia:</label>
+                    <select name="id_materia_calificar" id="id_materia_calificar">
+                        <?php
+                        // Consulta SQL para obtener la lista de materias desde la base de datos
+                        $query_materias = mysqli_query($conn, "SELECT id_materia, nombre FROM materias");
 
-    <!-- Formulario para actualizar calificaciones -->
-    <form action="../php/actualizar_calificacion.php" method="POST" enctype="multipart/form-data">
-        <!--<label for="boleta_calificar">Boleta del Alumno:</label>
-        <input type="text" name="boleta_calificar" id="boleta_calificar" placeholder="Ingrese la boleta del alumno">
+                        // Iterar sobre los resultados y generar opciones del menú desplegable
+                        while ($row_materia = mysqli_fetch_array($query_materias)) {
+                            echo "<option value='" . $row_materia['id_materia'] . "'>" . $row_materia['nombre'] . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
 
-        <label for="nombre_materia_calificar">Nombre de la Materia:</label>
-        <input type="text" name="id_materia_calificar" id="id_materia_calificar" placeholder="Ingrese el nombre de la materia"> -->
+            <label for="calificacion_parcial1">Calificación Parcial 1:</label>
+            <input type="text" name="calificacion_parcial1" id="calificacion_parcial1" placeholder="Ingrese la calificación del parcial 1">
 
+            <label for="calificacion_parcial2">Calificación Parcial 2:</label>
+            <input type="text" name="calificacion_parcial2" id="calificacion_parcial2" placeholder="Ingrese la calificación del parcial 2">
 
-        <label for="boleta_calificar">Boleta del Alumno:</label>
-        <select name="boleta_calificar" id="boleta_calificar">
-            <?php
-                // Consulta SQL para obtener la lista de boletas desde la tabla alumnos
-                $query_boletas = mysqli_query($conn, "SELECT boleta FROM alumnos");
+            <label for="calificacion_parcial3">Calificación Parcial 3:</label>
+            <input type="text" name="calificacion_parcial3" id="calificacion_parcial3" placeholder="Ingrese la calificación del parcial 3">
 
-                // Iterar sobre los resultados y generar opciones del menú desplegable
-                while($row_boleta = mysqli_fetch_array($query_boletas)) {
-                    echo "<option value='".$row_boleta['boleta']."'>".$row_boleta['boleta']."</option>";
-                }
-            ?>
-        </select>
-
-        <label for="id_materia_calificar">Nombre de la Materia:</label>
-        <select name="id_materia_calificar" id="id_materia_calificar">
-            <?php
-                // Consulta SQL para obtener la lista de materias desde la base de datos
-                $query_materias = mysqli_query($conn, "SELECT id_materia, nombre FROM materias");
-
-                // Iterar sobre los resultados y generar opciones del menú desplegable
-                while($row_materia = mysqli_fetch_array($query_materias)) {
-                    echo "<option value='".$row_materia['id_materia']."'>".$row_materia['nombre']."</option>";
-                }
-            ?>
-        </select>
-
-        <label for="calificacion_parcial1">Calificación Parcial 1:</label>
-        <input type="text" name="calificacion_parcial1" id="calificacion_parcial1" placeholder="Ingrese la calificación del parcial 1">
-
-        <label for="calificacion_parcial2">Calificación Parcial 2:</label>
-        <input type="text" name="calificacion_parcial2" id="calificacion_parcial2" placeholder="Ingrese la calificación del parcial 2">
-
-        <label for="calificacion_parcial3">Calificación Parcial 3:</label>
-        <input type="text" name="calificacion_parcial3" id="calificacion_parcial3" placeholder="Ingrese la calificación del parcial 3">
-
-        <button type="submit" name="btn_actualizar_calificacion">Actualizar Calificación</button>
-    </form>
+            <button type="submit" name="btn_actualizar_calificacion">Actualizar Calificación</button>
+            <p class="message"><?php if (isset($_GET['asignacion_result'])) {
+                                    echo $_GET['asignacion_result'];
+                                } ?></p>
+        </form>
+    </div>
 
 </body>
+
 </html>
+
+<?php
+// Cerrar la conexión
+$conn->close();
+?>
